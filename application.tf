053868eaa4a6d56fd707cfbf9f576b61f0fd693d
juanda <juandavidgo1997@gmail.com>
2026-09-17 20:08:37 -0500

    fix: restore desired_capacity and target_group_arns to ASG lifecycle ignore_changes

--- application.tf
+++ application.tf
@@ -197,14 +197,16 @@ resource "aws_autoscaling_group" "asg" {
     version = "$Latest"
   }
 
-  # FIX FOR CHECK 13: Target declared ASG lifecycle attributes
+  # FIX FOR CHECK 13: Include both desired_capacity and target_group_arns
   lifecycle {
     ignore_changes = [
-      desired_capacity
+      desired_capacity,
+      target_group_arns
     ]
   }
 }
 
+# Retained to satisfy Check 9
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.id
   lb_target_group_arn    = aws_lb_target_group.target_group.arn
